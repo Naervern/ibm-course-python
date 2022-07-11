f309bcfd6c74738ee6b49de786e77260b97316cb
Antonio Maximiano <50778647+Naervern@users.noreply.github.com>
2022-07-11 13:49:50 -0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -1,2 +1,15 @@
 # This is a python file for the IBM DevOps course
 # Maybe something will go here eventually, who knows?
+# Anyways, this is one of the stupidest ways to print Hello, world! that I can think of right now
+
+import time
+
+for i in "Hello, world!":
+    
+    for c in range(60, 123):
+        print(chr(c), end= "")
+        time.sleep(0.05)
+        if chr(c)==i:
+            break
+        else:
+            print("\b", end="")
